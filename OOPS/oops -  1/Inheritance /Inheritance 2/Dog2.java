894a779f822ec76f3a3db4bfa7b357ff96a7d61c
Anmol Tuteja <tutejaanmol54@gmail.com>
2024-01-02 01:29:19 +0530

    inheritance

--- OOPS/oops -  1/Inheritance /Inheritance 2/Dog2.java
+++ OOPS/oops -  1/Inheritance /Inheritance 2/Dog2.java
@@ -1,3 +1,17 @@
+// Overriding a method is when you create a method on a subclass, which has the same signature as a method on a super class.
+// You override a parent class method, when you want the child class to show different behavior for that method.
+
+// The overridden method can do one of three things: 
+// It can implement completely different behavior, overriding the behavior of the parent.
+// It can simply call the parent class's method, which is somewhat redundant to do.
+//example :
+//  @Override
+//  public void move(String speed) {
+//         super.move(speed);
+//     }
+
+// Or the method can call the parent class's method, and include other code to run, 
+//so it can extend the functionality for the Dog, for that behavior.
 
 public class Dog2 extends Animal2 {
     private String earShape;
@@ -14,6 +28,9 @@ public class Dog2 extends Animal2 {
     public Dog2(String type, double weight, String earShape, String tailShape) {
         super(type, weight < 15 ? "small" : (weight < 35 ? "medium" : "large"),
                 weight);
+        // weight < 15 ? "small" : (weight < 35 ? "medium" : "large") doing this here as
+        // can't do this before super() as supper must be first in constructor
+        // using expression is a way to deal with such problems
         this.earShape = earShape;
         this.tailShape = tailShape;
     }
@@ -24,6 +41,9 @@ public class Dog2 extends Animal2 {
                 "earShape='" + earShape + '\'' +
                 ", tailShape='" + tailShape + '\'' +
                 "} " + super.toString();
+
+        // super.toString() --> calling toString method of superclass (Animal) in
+        // subclass(Dog)
     }
 
     public void makeNoise() {
--- OOPS/oops -  1/Inheritance /Inheritance 2/Inheritance2Main.java
+++ OOPS/oops -  1/Inheritance /Inheritance 2/Inheritance2Main.java
@@ -4,15 +4,35 @@ public class Inheritance2Main {
 
         Animal2 animal = new Animal2("Generic Animal", "Huge", 400);
         doAnimalStuff(animal, "slow");
+        // Generic Animal makes some kind of noise
+        // Generic Animal moves slow
+        // Animal{type='Generic Animal', size='Huge', weight=400.0}
+        // _ _ _ _
 
         Dog2 dog = new Dog2();
         doAnimalStuff(dog, "fast");
+        // Mutt moves fast
+        // Dogs walk, run and wag their tail
+        // Dog{earShape='null', tailShape='null'} Animal{type='Mutt', size='Big',
+        // weight=50.0}
+        // _ _ _ _
 
         Dog2 yorkie = new Dog2("Yorkie", 15);
         doAnimalStuff(yorkie, "fast");
+        // Yorkie moves fast
+        // Dogs walk, run and wag their tail
+        // Dog{earShape='Perky', tailShape='Curled'} Animal{type='Yorkie',
+        // size='medium', weight=15.0}
+        // _ _ _ _
+
         Dog2 retriever = new Dog2("Labrador Retriever", 65,
                 "Floppy", "Swimmer");
         doAnimalStuff(retriever, "slow");
+        // Labrador Retriever moves slow
+        // Dogs walk, run and wag their tail
+        // Dog{earShape='Floppy', tailShape='Swimmer'} Animal{type='Labrador Retriever',
+        // size='large', weight=65.0}
+        // _ _ _ _
     }
 
     public static void doAnimalStuff(Animal2 animal, String speed) {
