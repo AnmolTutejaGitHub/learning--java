b59e1a9ae220ac17168348861b2e17eeab6502d1
Anmol Tuteja <tutejaanmol54@gmail.com>
2024-01-02 03:36:55 +0530

    java.lang.Object

--- OOPS/oops -  1/Inheritance /java.lang.Object/hashCodeInJava.java
+++ OOPS/oops -  1/Inheritance /java.lang.Object/hashCodeInJava.java
@@ -2,7 +2,9 @@ public class hashCodeInJava {
     public static void main(String[] args) {
 
         Students max = new Students("Max", 21);
-        System.out.println(max);
+        System.out.println(max.toString()); // Student@65ab7765
+        // 65ab7765 this code in toSting method is hashcode of the object
+
     }
 
     class Students {
