0cec81576e15093807959a358b08b965aa0b637c
Anmol Tuteja <tutejaanmol54@gmail.com>
2024-01-02 23:38:20 +0530

    string methods

---
+++ Strings/StringMethods.java
@@ -0,0 +1,84 @@
+public class StringMethods {
+    public static void main(String[] args) {
+
+        printInformation("Hello World");
+        printInformation("");
+        printInformation("\t   \n");
+
+        String helloWorld = "Hello World";
+        System.out.printf("index of r = %d %n", helloWorld.indexOf('r'));
+        System.out.printf("index of World = %d %n", helloWorld.indexOf("World"));
+
+        System.out.printf("index of l = %d %n", helloWorld.indexOf('l'));
+        System.out.printf("index of l = %d %n", helloWorld.lastIndexOf('l'));
+
+        System.out.printf("index of l = %d %n", helloWorld.indexOf('l',
+                3));
+        System.out.printf("index of l = %d %n", helloWorld.lastIndexOf('l',
+                8));
+
+        String helloWorldLower = helloWorld.toLowerCase();
+        if (helloWorld.equals(helloWorldLower)) {
+            System.out.println("Values match exactly");
+        }
+        if (helloWorld.equalsIgnoreCase(helloWorldLower)) {
+            System.out.println("Values match ignoring case");
+        }
+
+        if (helloWorld.startsWith("Hello")) {
+            System.out.println("String starts with Hello");
+        }
+        if (helloWorld.endsWith("World")) {
+            System.out.println("String ends with World");
+        }
+        if (helloWorld.contains("World")) {
+            System.out.println("String contains World");
+        }
+
+        if (helloWorld.contentEquals("Hello World")) {
+            System.out.println("Values match exactly");
+        }
+    }
+
+    public static void printInformation(String string) {
+
+        int length = string.length();
+        System.out.printf("Length = %d %n", length);
+
+        if (string.isEmpty()) {
+            System.out.println("String is Empty");
+            return;
+        }
+
+        if (string.isBlank()) {
+            System.out.println("String is Blank");
+        }
+
+        System.out.printf("First char = %c %n", string.charAt(0));
+
+        System.out.printf("Last char = %c %n", string.charAt(length - 1));
+    }
+
+    // output:
+    // Length = 11
+    // First char = H
+    // Last char = d
+    // Length = 0
+    // String is Empty
+    // Length = 5
+    // String is Blank
+    // First char =
+    // Last char =
+
+    // index of r = 8
+    // index of World = 6
+    // index of l = 2
+    // index of l = 9
+    // index of l = 3
+    // index of l = 3
+    // Values match ignoring case
+    // String starts with Hello
+    // String ends with World
+    // String contains World
+    // Values match exactly
+}
