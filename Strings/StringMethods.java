b934ecbdf4895f94abccb0146d175d705257a6b3
Anmol Tuteja <tutejaanmol54@gmail.com>
2024-01-03 01:45:55 +0530

    string methods

--- Strings/StringMethods.java
+++ Strings/StringMethods.java
@@ -1,3 +1,27 @@
+// The String has over 60 methods available. 
+// The String is a sequence of characters, meaning its characters are ordered and indexed. 
+
+// We can split String's methods up into three basic categories:
+// String Inspection Methods.:  they provide some info about the string  
+// Methods for Comparing String values. : compares 2 strings , usually returns a boolean 
+// String Manipulation Methods : they transform one string value into another
+
+//String Inspection Methods : length,charAt,indexOf,lastindexOf , isEmpty,isBlank 
+
+//String comparison methods : 
+// contentEquals :Returns a boolean if the String's value is equal to the value of the argument passed. 
+//This method allows for arguments other than String, for any type that is a character sequence.
+
+// equals : Return a boolean if the String's value is equal to the value of the argument passed. 
+
+// equalsIgnoreCase : Return a boolean if the String's value is equal (ignoring case), to the value of the argument passed. 
+
+// contains : Returns a boolean if the String contains the argument passed.
+
+//endsWith , startsWith : These return a boolean, and are much like the contains method, but more specific to the placement of the argument in the String.
+
+// regionMatches : Returns a boolean, if defined sub-regions are matched.
+
 public class StringMethods {
     public static void main(String[] args) {
 
@@ -9,13 +33,13 @@ public class StringMethods {
         System.out.printf("index of r = %d %n", helloWorld.indexOf('r'));
         System.out.printf("index of World = %d %n", helloWorld.indexOf("World"));
 
-        System.out.printf("index of l = %d %n", helloWorld.indexOf('l'));
+        System.out.printf("index of l = %d %n", helloWorld.indexOf('l')); // gives index of first occurence
         System.out.printf("index of l = %d %n", helloWorld.lastIndexOf('l'));
 
         System.out.printf("index of l = %d %n", helloWorld.indexOf('l',
-                3));
+                3)); // 3
         System.out.printf("index of l = %d %n", helloWorld.lastIndexOf('l',
-                8));
+                8)); // 3 startng from 8th index and moving backwards and looking till it find l
 
         String helloWorldLower = helloWorld.toLowerCase();
         if (helloWorld.equals(helloWorldLower)) {
@@ -38,6 +62,9 @@ public class StringMethods {
         if (helloWorld.contentEquals("Hello World")) {
             System.out.println("Values match exactly");
         }
+
+        // contentEquals method can be used to compare a StringBuilder's value ,
+        // which the equals method doesn't support
     }
 
     public static void printInformation(String string) {
