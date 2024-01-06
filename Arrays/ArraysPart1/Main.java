b47eb3ca92d6d39071ebcd3a7e2518137f7628c0
Anmol Tuteja <tutejaanmol54@gmail.com>
2024-01-06 23:07:00 +0530

    array

--- Arrays/ArraysPart1/Main.java
+++ Arrays/ArraysPart1/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // The size of an array, once created, is fixed.
+        // We can't add or delete elements
         int[] myIntArray = new int[10]; // 10 -> size of array
         myIntArray[0] = 45;
         myIntArray[1] = 1;
@@ -17,13 +19,20 @@ public class Main {
 
         int theArray[] = new int[] { 1, 3, 68, 46 };
 
-        int[] firstTen = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        // Java allows us to drop new int[], from the expression, as we show here.
+        // This is known as an anonymous array.
+        int[] firstTen = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; // anonymous array.
+        // An anonymous array initializer, can only be used in a declaration statement.
+        // means can't do :
+        // int[] newArray;
+        // newArray={ 5, 4, 3, 2, 1 }; //error
         System.out.println("first = " + firstTen[0]); // first = 1
         int arrayLength = firstTen.length;
         System.out.println("length of array = " + arrayLength); // length of array = 10
         System.out.println("last = " + firstTen[arrayLength - 1]); // last = 10
 
         int[] newArray;
+        // newArray={ 5, 4, 3, 2, 1 }; //error
         newArray = new int[] { 5, 4, 3, 2, 1 };
         for (int i = 0; i < newArray.length; i++) {
             System.out.print(newArray[i] + " "); // 5 4 3 2 1
