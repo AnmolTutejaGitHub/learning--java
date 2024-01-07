d188c4b23ebf101a3b5882d442e6ad4c423dc6da
Anmol Tuteja <tutejaanmol54@gmail.com>
2024-01-08 00:19:58 +0530

    MaxSubarray sum

---
+++ Arrays/subarray/PrintMaxSubarraysSum.java
@@ -0,0 +1,69 @@
+// visit this que after doing / revising PrintSubarrays 
+
+// You are given an array of integers, arr. Your task is to find the contiguous subarray 
+// with the largest sum. A subarray is defined as a contiguous segment of the array.
+// Write a function maxSubarraySum that takes an array of integers as input and returns the sum of the maximum subarray.
+package subarray;
+
+public class PrintMaxSubarraysSum {
+
+    // bruteforce
+    public static void maxSubarraySum1(int arr[]) {
+        int largestSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                int currSum = 0;
+                for (int k = i; k <= j; k++) {
+                    currSum += arr[k];
+                }
+                largestSum = Math.max(largestSum, currSum);
+            }
+        }
+
+        System.out.println("max subarray sum is : " + largestSum);
+    }
+
+    // optimization1 : Prefix Sum array
+    public static void maxSubarraySum2(int arr[]) {
+        int maxSum = Integer.MIN_VALUE;
+
+        int prefix[] = new int[arr.length];
+        prefix[0] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                int currSum = i == 0 ? prefix[j] : prefix[j] - prefix[i - 1];
+                maxSum = Math.max(maxSum, currSum);
+            }
+        }
+
+        System.out.println("max subarray sum is : " + maxSum);
+    }
+
+    // optimization 2 : Kadane's Algorithm
+    public static void maxSubarraySum3(int arr[]) {
+        int cs = 0;
+        int ms = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            cs = cs + arr[i];
+            if (cs < 0) {
+                cs = 0;
+            }
+            ms = Math.max(ms, cs);
+        }
+
+        System.out.println("max subarray sum is : " + ms);
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 1, -2, 6, -1, 3 };
+        maxSubarraySum1(arr);
+        maxSubarraySum2(arr);
+        maxSubarraySum3(arr);
+    }
+}
