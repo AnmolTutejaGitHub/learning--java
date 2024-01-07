f28f72a59fa54c4493cfd8a59db1384e4b04490e
Anmol Tuteja <tutejaanmol54@gmail.com>
2024-01-07 17:52:53 +0530

    reference vs value type in java

---
+++ Arrays/RefercenceVsValueType/Main.java
@@ -0,0 +1,28 @@
+package RefercenceVsValueType;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] myIntArray = new int[5];
+        int[] anotherArray = myIntArray;
+
+        System.out.println("myIntArray = " + Arrays.toString(myIntArray)); // myIntArray = [0, 0, 0, 0, 0]
+        System.out.println("anotherArray = " + Arrays.toString(anotherArray)); // anotherArray = [0, 0, 0, 0, 0]
+
+        anotherArray[0] = 1;
+        modifyArray(myIntArray);
+
+        System.out.println("after change myIntArray = "
+                + Arrays.toString(myIntArray)); // after change myIntArray = [1, 2, 0, 0, 0]
+        System.out.println("after change anotherArray = " +
+                Arrays.toString(anotherArray)); // after change myIntArray = [1, 2, 0, 0, 0]
+    }
+
+    private static void modifyArray(int[] array) {
+
+        array[1] = 2;
+    }
+}
