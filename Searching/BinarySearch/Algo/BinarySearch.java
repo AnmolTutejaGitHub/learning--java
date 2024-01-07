d800a5fdaaee540e1ef182ab5cf98400a0fe6d9e
Anmol Tuteja <tutejaanmol54@gmail.com>
2024-01-07 22:36:41 +0530

    searching

---
+++ Searching/BinarySearch/Algo/BinarySearch.java
@@ -0,0 +1,39 @@
+package BinarySearch.Algo;
+
+// if mutiple elements in the array equal to target 
+
+public class BinarySearch {
+
+    static int findFirstOccurrence(int arr[], int target) {
+        int left = 0, right = arr.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                result = mid;
+                // Continue searching in the left subarray for the first occurrence
+                right = mid - 1;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] sortedArray = { 2, 5, 8, 12, 16, 16, 23, 38, 45, 56, 72 };
+        int targetElement = 16;
+
+        int result = findFirstOccurrence(sortedArray, targetElement);
+
+        if (result != -1)
+            System.out.println("First occurrence of " + targetElement + " found at index " + result);
+        else
+            System.out.println(targetElement + " not found in the array.");
+    }
+}
---
+++ Searching/LinearSearch.java
@@ -0,0 +1,25 @@
+
+public class LinearSearch {
+
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 2, 5, 8, 12, 16, 23, 38, 42, 50 };
+        int target = 16;
+
+        int result = linearSearch(arr, target);
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
+}
