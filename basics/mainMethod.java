4ac4307642dd5fd61bb12416833bf96ff1848a03
Anmol Tuteja <tutejaanmol54@gmail.com>
2024-01-08 12:23:01 +0530

    basics

---
+++ basics/mainMethod.java
@@ -0,0 +1,7 @@
+public class mainMethod {
+    // Both static and public are modifiers we can change their position
+
+    static public void main(String... args) {
+        System.out.println("wtf I put this in about main method section ");
+    }
+}
