5144f066094a0746029eede7d516209140ec3994
Anmol Tuteja <tutejaanmol54@gmail.com>
2024-01-08 19:44:02 +0530

    leetcode

---
+++ Leetcode/1480. Running Sum of 1d Array/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        // int first = nums[0];
+        int[] ans = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            // ans[i]=nums[0]; then start from j=1;
+            for (int j = 0; j <= i; j++) {
+                ans[i] += nums[j];
+            }
+        }
+        return ans;
+    }
+}
