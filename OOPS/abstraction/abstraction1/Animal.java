59b117d0c257b53b1e3b47a88a764d9b17ece796
Anmol Tuteja <tutejaanmol54@gmail.com>
2024-01-11 16:33:05 +0530

    abstraction1

---
+++ OOPS/abstraction/abstraction1/Animal.java
@@ -0,0 +1,19 @@
+package abstraction.abstraction1;
+
+public abstract class Animal {
+
+    protected String type;
+    private String size;
+    private double weight;
+
+    public Animal(String type, String size, double weight) {
+        this.type = type;
+        this.size = size;
+        this.weight = weight;
+    }
+
+    public abstract void move(String speed);
+
+    public abstract void makeNoise();
+
+}
---
+++ OOPS/abstraction/abstraction1/Main.java
@@ -0,0 +1,8 @@
+package abstraction.abstraction1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
