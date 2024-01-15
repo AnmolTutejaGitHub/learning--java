1d8313cde08a18f46f1080ebe3b3ec190570b25f
Anmol Tuteja <tutejaanmol54@gmail.com>
2024-01-15 11:15:00 +0530

    leetcode

---
+++ Leetcode/575. Distribute Candies/Solution.java
@@ -0,0 +1,20 @@
+
+//package Leetcode.575. Distribute Candies;
+import java.util.*;
+
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        Set<Integer> sol = new HashSet<>();
+        for (int ele : candyType) {
+            sol.add(ele);
+        }
+        if (sol.size() == candyType.length / 2)
+            return sol.size();
+        if (sol.size() < candyType.length / 2)
+            return sol.size();
+        if (sol.size() > candyType.length / 2)
+            return candyType.length / 2;
+
+        return -1;
+    }
+}
