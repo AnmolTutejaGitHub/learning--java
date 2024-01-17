ce00cc48d0d323f6ad100105b4e6857e3f9ce2d7
AnmolTuteja12 <anmol0115.be23@chitkara.edu.in>
2024-01-17 12:44:12 +0530

    leetcode

---
+++ Leetcode/1539. Kth Missing Positive Number/Solution.java
@@ -0,0 +1,23 @@
+//package Leetcode.1539. Kth Missing Positive Number;
+
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int ans = -1;
+        ArrayList<Integer> missing = new ArrayList<>();
+        boolean flag = false;
+        // for(int j=1;j<100 ; j++)
+        for (int j = 1; j < (arr.length + k + 1); j++) {
+            flag = false;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == j) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag)
+                missing.add(j);
+        }
+        System.out.println(missing);
+        return missing.get(k - 1);
+    }
+}
