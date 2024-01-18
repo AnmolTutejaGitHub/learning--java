1fb887f9734433419b3ed8099d9bbff6f8a72c00
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-01-18 23:26:47 +0530

    wild card

--- Generics/Basics/wild_card.java
+++ Generics/Basics/wild_card.java
@@ -1,6 +1,31 @@
 package Generics.Basics;
 
-// wild card in java 
-public class wild_card {
+import java.util.ArrayList;
+import java.util.List;
 
+// wild card in java 
+// example a list that can take any type of things
+
+public class wild_card {
+    public static void main(String[] args) {
+        List<Integer> intList = new ArrayList<>();
+        intList.add(1);
+        // printList1(intList); //error
+        // even though an integer is subclass of object , a list isn't subclass of list
+        // of object . This is why wildcard in needed
+
+        printList2(intList);
+    }
+
+    public static void printList1(List<Object> mylist) {
+        System.out.println(mylist);
+    }
+
+    public static void printList2(List<?> mylist) {
+        System.out.println(mylist);
+    }
+
+    // can also have bounded wild card
+    // can have public static void printList1(List<? extends Animal> mylist) {} //
+    // now method only takes list of animal or its subclass
 }
