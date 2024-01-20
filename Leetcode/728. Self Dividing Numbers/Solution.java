65699dee7da7dfe56c71817572e3147b3d8f4f0b
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-01-20 18:21:49 +0530

    leetcode

---
+++ Leetcode/728. Self Dividing Numbers/Solution.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        for (int i = left; i <= right; i++) {
+            if (isSelfDividing(i))
+                ans.add(i);
+        }
+        return ans;
+
+    }
+
+    public boolean isSelfDividing(int n) {
+        int cpyn = n;
+        int mod = 10;
+        // boolean flag =
+        while (cpyn != 0) {
+            int rem = cpyn % 10;
+            if (rem == 0)
+                return false;
+            // if(n%rem==0) flag = true ;
+            if (n != 0 && rem != 0 && n % rem != 0 && n != 0 && rem != 0)
+                return false;
+
+            cpyn = cpyn / 10;
+        }
+        return true;
+    }
+}
