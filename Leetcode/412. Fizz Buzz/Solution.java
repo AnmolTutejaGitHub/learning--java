b00dd8bb5ec29433fcdee70687e27d3452afaf40
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-01-27 14:54:19 +0530

    leetcode

---
+++ Leetcode/412. Fizz Buzz/Solution.java
@@ -0,0 +1,23 @@
+//package Leetcode.412. Fizz Buzz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> fizzBuzz(int n) {
+
+        List<String> ans = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0)
+                ans.add("FizzBuzz");
+            else if (i % 3 == 0)
+                ans.add("Fizz");
+            else if (i % 5 == 0)
+                ans.add("Buzz");
+            else
+                ans.add("" + i);
+        }
+        return ans;
+    }
+}
