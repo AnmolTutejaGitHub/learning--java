0d12ea4c426271585401e9e4977a14961bf47438
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-01-28 16:15:29 +0530

    binary right shift

---
+++ bit_manipulation/binary_right_shift.java
@@ -0,0 +1,10 @@
+package bit_manipulation;
+
+public class binary_right_shift {
+    public static void main(String[] args) {
+        System.out.println(6 >> 1); // 3
+
+        // formula :
+        // a >> b = a / (2 power b)
+    }
+}
