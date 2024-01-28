199deaaef1c80bccef3ef872d214da3945786665
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-01-28 16:03:48 +0530

    binary not

--- bit_manipulation/binary_NOT.java
+++ bit_manipulation/binary_NOT.java
@@ -1,7 +1,10 @@
 package bit_manipulation;
 
+// binary one's complement 
+// binary notoperator 
 public class binary_NOT {
     public static void main(String[] args) {
-        System.out.println(~5);
+        System.out.println(~5); // -6
+        System.out.println(~0); // -1
     }
 }
