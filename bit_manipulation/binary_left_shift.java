c8708bfbaaf384c43eaeda6c8e387bb3d572ba1a
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-01-28 16:10:40 +0530

    binary left shift

---
+++ bit_manipulation/binary_left_shift.java
@@ -0,0 +1,11 @@
+package bit_manipulation;
+
+// binary left shift 
+public class binary_left_shift {
+    public static void main(String[] args) {
+        System.out.println(5 << 2); // 20
+
+        // formula : a<<b = a* (2 power b)
+    }
+
+}
