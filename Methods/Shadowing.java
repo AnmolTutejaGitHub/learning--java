fdd62fd287594e55f9efb0c495cfcb540723ad4d
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-01-29 19:15:03 +0530

    shadowing in java

--- Methods/Shadowing.java
+++ Methods/Shadowing.java
@@ -2,12 +2,18 @@ package Methods;
 
 public class Shadowing {
     static int x = 90;
+    static int y = 68;
 
     public static void main(String[] args) {
         System.out.println(x); // 90
         int x = 40; // shadowing
         System.out.println(x); // 40
         func();// 90
+
+        int y;
+        // System.out.println(y); // error : y needs to be intitialised scope /
+        // shadowing begins from above line
+        y = 90;
 
     }
 
