93666e5965418301a9267b61e21edf3503b4620c
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-01-31 19:45:24 +0530

    leetcode

---
+++ Leetcode/1346. Check If N and Its Double Exist/Solution.java
@@ -0,0 +1,13 @@
+//package Leetcode.1346. Check If N and Its Double Exist;
+
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (i != j && arr[i] == 2 * arr[j])
+                    return true;
+            }
+        }
+        return false;
+    }
+}
