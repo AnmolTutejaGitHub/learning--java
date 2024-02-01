0cefeaff5c87f626d989434f9f11d2ecbd9fe686
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-02-01 14:55:59 +0530

    leetcode

---
+++ Leetcode/1704. Determine if String Halves Are Alike/Solution.java
@@ -0,0 +1,22 @@
+//package Leetcode.1704. Determine if String Halves Are Alike;
+
+class Solution {
+    public boolean halvesAreAlike(String s) {
+        String a = s.substring(0, s.length() / 2);
+        String b = s.substring(s.length() / 2, s.length());
+
+        return countVowels(a) == countVowels(b);
+
+    }
+
+    static int countVowels(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o'
+                    || str.charAt(i) == 'u' || str.charAt(i) == 'A' || str.charAt(i) == 'E' || str.charAt(i) == 'I'
+                    || str.charAt(i) == 'O' || str.charAt(i) == 'U')
+                count++;
+        }
+        return count;
+    }
+}
