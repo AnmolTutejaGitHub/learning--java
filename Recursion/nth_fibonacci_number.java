1842e2437fe5cbee5ac02d66f5928e315b13ca55
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-02-01 19:31:51 +0530

    recursion

---
+++ Recursion/nth_fibonacci_number.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class nth_fibonacci_number {
+    public static void main(String[] args) {
+        System.out.println(fibo(6)); // 8 // 6th fibonacci number is 8
+
+    }
+
+    public static int fibo(int n) {
+
+        // base condition :
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+
+        return fibo(n - 1) + fibo(n - 2);
+    }
+}
