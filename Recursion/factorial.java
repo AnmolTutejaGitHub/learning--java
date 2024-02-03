c5abdc949f1b2d3c11b7c53fefbf04764b32e958
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-02-03 15:46:10 +0530

    fact

---
+++ Recursion/factorial.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class factorial {
+    public static void main(String[] args) {
+        System.out.println(fact(5)); // 120
+    }
+
+    public static int fact(int n) {
+
+        // base condition
+        if (n == 0)
+            return 1;
+
+        return n * fact(n - 1);
+    }
+}
