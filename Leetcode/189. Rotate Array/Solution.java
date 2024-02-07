2667bb7fbc6eba84c1e20a3462a9811b3a114c23
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-02-07 21:42:50 +0530

    leetcode : array rotation

---
+++ Leetcode/189. Rotate Array/Solution.java
@@ -0,0 +1,25 @@
+//package Leetcode.189. Rotate Array;
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        if (nums.length == 1)
+            return;
+        int n = nums.length;
+        k = k % n;
+
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+
+    }
+
+    static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
