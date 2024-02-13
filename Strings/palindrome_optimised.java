45c1e98a701e6402a99c735ab3c943e14f047044
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-02-13 10:05:47 +0530

    palindrome

---
+++ Strings/palindrome_optimised.java
@@ -0,0 +1,19 @@
+// optimised isplaindrome 
+public class palindrome_optimised {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right))
+                return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+}
