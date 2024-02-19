06e3ebc6719a2a1b7e49b9bd016c99a2e19d0432
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-02-19 17:49:00 +0530

    optimised using dutch national flag algorithm

--- Leetcode/75. Sort Colors/Solution.java
+++ Leetcode/75. Sort Colors/Solution.java
@@ -1,17 +1,38 @@
 //package Leetcode.75. Sort Colors;
-
+// dutch national flag algorithm 
 class Solution {
     public void sortColors(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if (j + 1 < nums.length && nums[j] > nums[j + 1]) {
+        // for(int i=0;i<nums.length;i++){
+        // for(int j=0;j<nums.length;j++){
+        // if(j+1<nums.length && nums[j]>nums[j+1]){
 
-                    int temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = temp;
-                }
+        // int temp = nums[j];
+        // nums[j] = nums[j+1];
+        // nums[j+1] = temp;
+        // }
+        // }
+        // }
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else if (nums[mid] == 2) {
+                swap(nums, mid, high);
+                high--;
             }
         }
-        // return nums;
+    }
+
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
