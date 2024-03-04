707db3e3a334f5bf6672c574e11c2dc17ec7def0
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-03-04 21:15:37 +0530

    leetcode

---
+++ Leetcode/237. Delete Node in a Linked List/Solution.java
@@ -0,0 +1,18 @@
+//package Leetcode.237. Delete Node in a Linked List;
+
+class Solution {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
