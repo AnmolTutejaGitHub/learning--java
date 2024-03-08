4e1c0904fa55da9b0ed0d31a013d25b61b2c1739
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-03-08 18:10:53 +0530

    leetcode

---
+++ Leetcode/143. Reorder List/Solution.java
@@ -0,0 +1,62 @@
+//package Leetcode.143. Reorder List;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public void reorderList(ListNode head) {
+        ListNode mid = middle(head);
+
+        ListNode curr = mid;
+        ListNode prev = null;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        ListNode first = head;
+        ListNode second = prev;
+
+        while (second.next != null && first.next != null) {
+            ListNode temp = first.next;
+            first.next = second;
+            first = temp;
+
+            temp = second.next;
+            second.next = first;
+            second = temp;
+        }
+
+    }
+
+    public ListNode middle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+}
