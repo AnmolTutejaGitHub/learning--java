c37867fbce8371587150cfc5f04a5e04ea8e641e
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-03-14 15:23:20 +0530

    optimised using stack

--- Leetcode/20. Valid Parentheses/Solution.java
+++ Leetcode/20. Valid Parentheses/Solution.java
@@ -1,25 +1,65 @@
 //package Leetcode.20. Valid Parentheses;
 
+// class Solution {
+//     public boolean isValid(String s) {
+//         boolean pass = true;
+//         while (pass && s.length() != 0) {
+//             pass = false;
+//             if (s.contains("()")) {
+//                 s = s.replace("()", "");
+//                 pass = true;
+//             }
+//             if (s.contains("[]")) {
+//                 s = s.replace("[]", "");
+//                 pass = true;
+//             }
+//             if (s.contains("{}")) {
+//                 s = s.replace("{}", "");
+//                 pass = true;
+//             }
+//             if (!pass)
+//                 break;
+//         }
+//         return s.length() == 0;
+//     }
+// }
+
+import java.util.Stack;
+
 class Solution {
     public boolean isValid(String s) {
-        boolean pass = true;
-        while (pass && s.length() != 0) {
-            pass = false;
-            if (s.contains("()")) {
-                s = s.replace("()", "");
-                pass = true;
+        Stack<Character> stack = new Stack<>();
+        char prev;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (stack.isEmpty())
+                    return false;
+
+                prev = stack.pop();
+                if (prev != '(')
+                    return false;
             }
-            if (s.contains("[]")) {
-                s = s.replace("[]", "");
-                pass = true;
+
+            else if (s.charAt(i) == '}') {
+                if (stack.isEmpty())
+                    return false;
+
+                prev = stack.pop();
+                if (prev != '{')
+                    return false;
             }
-            if (s.contains("{}")) {
-                s = s.replace("{}", "");
-                pass = true;
-            }
-            if (!pass)
-                break;
+
+            else if (s.charAt(i) == ']') {
+                if (stack.isEmpty())
+                    return false;
+
+                prev = stack.pop();
+                if (prev != '[')
+                    return false;
+            } else
+                stack.push(s.charAt(i));
         }
-        return s.length() == 0;
+
+        return stack.isEmpty();
     }
 }
