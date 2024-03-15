243d7b6adfa0d96a7b79f31d04a411c75740b6fd
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-03-15 13:04:02 +0530

    leetcode : optimised

--- Leetcode/88. Merge Sorted Array/Solution.java
+++ Leetcode/88. Merge Sorted Array/Solution.java
@@ -1,15 +1,43 @@
 //package Leetcode.88. Merge Sorted Array;
 
-import java.util.Arrays;
+//import java.util.Arrays;
+
+// class Solution {
+//     public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+//         int j = 0;
+//         for (int i = 0; i < n; i++) {
+//             nums1[i + m] = nums2[i];
+//         }
+
+//         Arrays.sort(nums1);
+//     }
+// }
 
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int ans[] = new int[m + n];
 
+        int i = 0;
         int j = 0;
-        for (int i = 0; i < n; i++) {
-            nums1[i + m] = nums2[i];
+        int idx = 0;
+        if (n == 0 || n > nums2.length)
+            return;
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j])
+                ans[idx++] = nums1[i++];
+            else
+                ans[idx++] = nums2[j++];
+        }
+        while (i < m) {
+            ans[idx++] = nums1[i++];
+        }
+        while (j < n) {
+            ans[idx++] = nums2[j++];
         }
 
-        Arrays.sort(nums1);
+        i = 0;
+        while (i < nums1.length)
+            nums1[i] = ans[i++];
     }
 }
