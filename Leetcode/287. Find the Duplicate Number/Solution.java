c8cbd2f08df9803e67f62dffcf16edd11e25ef0b
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-03-24 14:26:58 +0530

    leetcode : optimised

--- Leetcode/287. Find the Duplicate Number/Solution.java
+++ Leetcode/287. Find the Duplicate Number/Solution.java
@@ -1,28 +1,41 @@
 //package Leetcode.287. Find the Duplicate Number;
 
+// // cyclic sort
+// class Solution {
+//     public int findDuplicate(int[] nums) {
+//         cyclicSort(nums);
+
+//         for (int i = 0; i < nums.length; i++) {
+//             if (nums[i] != i + 1)
+//                 return nums[i];
+//         }
+
+//         return -1;
+//     }
+
+//     public static void cyclicSort(int[] arr) {
+//         int i = 0;
+//         while (i < arr.length) {
+//             int correctInx = arr[i] - 1;
+//             if (arr[i] != arr[correctInx]) {
+//                 int temp = arr[i];
+//                 arr[i] = arr[correctInx];
+//                 arr[correctInx] = temp;
+//             } else {
+//                 i++;
+//             }
+//         }
+//     }
+// }
+
 class Solution {
     public int findDuplicate(int[] nums) {
-        cyclicSort(nums);
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != i + 1)
-                return nums[i];
+        boolean arr[] = new boolean[nums.length];
+        for (int ele : nums) {
+            if (arr[ele])
+                return ele;
+            arr[ele] = true;
         }
-
         return -1;
-    }
-
-    public static void cyclicSort(int[] arr) {
-        int i = 0;
-        while (i < arr.length) {
-            int correctInx = arr[i] - 1;
-            if (arr[i] != arr[correctInx]) {
-                int temp = arr[i];
-                arr[i] = arr[correctInx];
-                arr[correctInx] = temp;
-            } else {
-                i++;
-            }
-        }
     }
 }
