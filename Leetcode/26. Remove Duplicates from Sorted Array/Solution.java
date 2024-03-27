5f578f6eac5e2dc0c6488d74833f655bb24f6f7f
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-03-27 13:43:25 +0530

    leetcode

---
+++ Leetcode/26. Remove Duplicates from Sorted Array/Solution.java
@@ -0,0 +1,14 @@
+//package Leetcode.26. Remove Duplicates from Sorted Array;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+}
