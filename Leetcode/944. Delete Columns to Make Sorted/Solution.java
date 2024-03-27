ac769a0cf3c5818e383363eaec48d986912ad89b
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-03-28 01:22:33 +0530

    leetcode

---
+++ Leetcode/944. Delete Columns to Make Sorted/Solution.java
@@ -0,0 +1,16 @@
+//package Leetcode.944. Delete Columns to Make Sorted;
+
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int count = 0;
+        for (int i = 0; i < strs[0].length(); i++) {
+            for (int j = 1; j < strs.length; j++) {
+                if (strs[j].charAt(i) < strs[j - 1].charAt(i)) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
