8f6eb9834f5d8cd73ca932fd5d9aea0b6d6c6504
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-03-27 14:24:55 +0530

    leetcode : optimised

--- Leetcode/1991. Find the Middle Index in Array/Solution.java
+++ Leetcode/1991. Find the Middle Index in Array/Solution.java
@@ -1,26 +1,44 @@
+// class Solution {
+//     public int findMiddleIndex(int[] nums) {
+
+//         int middleIndex = -1;
+//         int ls = 0;
+//         // int rs=0;
+//         int ts = 0;
+//         for (int i = 0; i < nums.length; i++) {
+//             ts += nums[i];
+//         }
+
+//         for (int i = 0; i < nums.length; i++) {
+//             ls = 0;
+//             // rs=0;
+//             // ts+=nums[i];
+//             for (int j = 0; j < i; j++) {
+//                 ls += nums[j];
+//                 // rs+=nums[nums.length-j-1];
+//             }
+
+//             if ((ts - ls) - ls - nums[i] == 0)
+//                 return middleIndex = i;
+//         }
+//         return middleIndex;
+//     }
+// }
+
 class Solution {
     public int findMiddleIndex(int[] nums) {
-
-        int middleIndex = -1;
-        int ls = 0;
-        // int rs=0;
         int ts = 0;
         for (int i = 0; i < nums.length; i++) {
             ts += nums[i];
         }
 
+        int prefix = 0;
         for (int i = 0; i < nums.length; i++) {
-            ls = 0;
-            // rs=0;
-            // ts+=nums[i];
-            for (int j = 0; j < i; j++) {
-                ls += nums[j];
-                // rs+=nums[nums.length-j-1];
-            }
-
-            if ((ts - ls) - ls - nums[i] == 0)
-                return middleIndex = i;
+            if (ts - prefix - nums[i] == prefix)
+                return i;
+            prefix += nums[i];
         }
-        return middleIndex;
+        return -1;
+
     }
 }
