da35ff021d6b847831eadb399ef1018584ba00a2
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-03-28 01:09:54 +0530

    leetcode optimised

--- Leetcode/520. Detect Capital/Solution.java
+++ Leetcode/520. Detect Capital/Solution.java
@@ -2,16 +2,11 @@
 
 class Solution {
     public boolean detectCapitalUse(String word) {
-
-        if (word.equals(word.toUpperCase()))
-            return true;
-        else if (word.equals(word.toLowerCase()))
-            return true;
-        else if ((word.charAt(0) + "" + word.substring(1, word.length()))
-                .equals(((word.charAt(0) + "").toUpperCase() + (word.substring(1, word.length())).toLowerCase())))
+        if (word.equals(word.toUpperCase()) ||
+                word.equals(word.toLowerCase()) ||
+                word.substring(1).equals(word.substring(1).toLowerCase()))
             return true;
 
         return false;
-
     }
 }
