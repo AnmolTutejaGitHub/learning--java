40e6fa1aad2f5fa1b62d801aa7546f77b3348b65
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-04-01 12:23:59 +0530

    optimised leetcode POTD

--- Leetcode/58. Length of Last Word/Solution.java
+++ Leetcode/58. Length of Last Word/Solution.java
@@ -1,10 +1,23 @@
 //package Leetcode.58. Length of Last Word;
 
+// class Solution {
+//     public int lengthOfLastWord(String s) {
+//         String[] str = s.split("\\s+");
+
+//         return str[str.length - 1].length();
+
+//     }
+// }
+
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] str = s.split("\\s+");
-
-        return str[str.length - 1].length();
-
+        int length = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ')
+                length++;
+            else if (s.charAt(i) == ' ' && length != 0)
+                break;
+        }
+        return length;
     }
 }
