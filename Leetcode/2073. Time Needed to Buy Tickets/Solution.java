e63ed69bd7ba159805b196e99300f55c5e6242a4
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-04-09 13:58:59 +0530

    leetcode

--- Leetcode/2073. Time Needed to Buy Tickets/Solution.java
+++ Leetcode/2073. Time Needed to Buy Tickets/Solution.java
@@ -2,19 +2,27 @@
 
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
-        int pass = 0;
-        while (tickets[k] > 0) {
-            for (int j = 0; j < tickets.length; j++) {
-                if (tickets[j] > 0) {
-                    tickets[j]--;
-                    pass++;
-                }
+        // int pass =0;
+        // while(tickets[k]>0){
+        // for(int j=0;j<tickets.length;j++){
+        // if(tickets[j]>0) {tickets[j]--;
+        // pass++;}
 
-                if (j == k && tickets[j] == 0)
-                    break;
+        // if(j==k && tickets[j]==0) break;
 
-            }
+        // }
+        // }
+        // return pass;
+
+        int ans = 0;
+        for (int i = 0; i < tickets.length; i++) {
+            if (i < k)
+                ans += Math.min(tickets[i], tickets[k]);
+            if (i > k)
+                ans += Math.min(tickets[i], tickets[k] - 1);
         }
-        return pass;
+        ans += tickets[k];
+
+        return ans;
     }
 }
