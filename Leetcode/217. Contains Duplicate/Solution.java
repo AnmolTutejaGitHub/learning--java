ca120bd41d34c3a12a3ba6ee465edbd53b05aba3
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-04-11 13:11:31 +0530

    leetcode

---
+++ Leetcode/217. Contains Duplicate/Solution.java
@@ -0,0 +1,16 @@
+//package Leetcode.217. Contains Duplicate;
+
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int n : nums) {
+            if (set.contains(n))
+                return true;
+            set.add(n);
+        }
+        return false;
+    }
+}
