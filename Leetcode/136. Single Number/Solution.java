bba05aac1ee4fbea6c299a3ba9424cfe0536ac38
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-04-17 21:56:34 +0530

    leetcode

---
+++ Leetcode/136. Single Number/Solution.java
@@ -0,0 +1,11 @@
+//package Leetcode.136. Single Number;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            res = res ^ nums[i];
+        }
+        return res;
+    }
+}
