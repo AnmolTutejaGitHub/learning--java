59cb34d827c149640830c8cd83c670499e1c0f04
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-04-20 21:59:53 +0530

    leetcode

---
+++ Leetcode/1903. Largest Odd Number in String/Solution.java
@@ -0,0 +1,11 @@
+//package Leetcode.1903. Largest Odd Number in String;
+
+class Solution {
+    public String largestOddNumber(String num) {
+        for (int i = num.length() - 1; i >= 0; i--) {
+            if ((num.charAt(i) - '0') % 2 != 0)
+                return num.substring(0, i + 1);
+        }
+        return "";
+    }
+}
