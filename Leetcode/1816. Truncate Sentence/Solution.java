0de90122cdeff9b8c56d716dcb9f54c9cd2327d8
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-04-20 22:27:38 +0530

    leetcode optimised

--- Leetcode/1816. Truncate Sentence/Solution.java
+++ Leetcode/1816. Truncate Sentence/Solution.java
@@ -1,17 +1,33 @@
 //package Leetcode.1816. Truncate Sentence;
 
+// class Solution {
+//     public String truncateSentence(String s, int k) {
+//         String[] arr = s.split(" ");
+//         String ans = "";
+
+//         for (int i = 0; i < k; i++) {
+//             if (i == 0)
+//                 ans += arr[i];
+//             else
+//                 ans += " " + arr[i];
+//         }
+//         return ans;
+
+//     }
+// }
+
 class Solution {
     public String truncateSentence(String s, int k) {
         String[] arr = s.split(" ");
-        String ans = "";
+        StringBuilder ans = new StringBuilder();
 
         for (int i = 0; i < k; i++) {
             if (i == 0)
-                ans += arr[i];
+                ans.append(arr[i]);
             else
-                ans += " " + arr[i];
+                ans.append(" ").append(arr[i]);
         }
-        return ans;
+        return ans.toString();
 
     }
 }
