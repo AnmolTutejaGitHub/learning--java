cfd95d45106c0fea2e01806f380c1a0ddfb66aba
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-04-22 19:30:47 +0530

    leetcode : optimised

--- Leetcode/414. Third Maximum Number/Solution.java
+++ Leetcode/414. Third Maximum Number/Solution.java
@@ -2,20 +2,48 @@
 
 import java.util.Arrays;
 
+// class Solution {
+//     public int thirdMax(int[] nums) {
+//         Arrays.sort(nums);
+//         int l = nums[nums.length - 1];
+//         if (nums.length <= 2)
+//             return l;
+
+//         int count = 1;
+//         for (int i = nums.length - 2; i >= 0; i--) {
+//             if (nums[i] != nums[i + 1])
+//                 count++;
+//             if (count == 3)
+//                 return nums[i];
+//         }
+//         return l;
+//     }
+// }
+
 class Solution {
     public int thirdMax(int[] nums) {
-        Arrays.sort(nums);
-        int l = nums[nums.length - 1];
-        if (nums.length <= 2)
-            return l;
+        long l = Long.MIN_VALUE;
+        long sl = Long.MIN_VALUE;
+        long tl = Long.MIN_VALUE;
 
-        int count = 1;
-        for (int i = nums.length - 2; i >= 0; i--) {
-            if (nums[i] != nums[i + 1])
-                count++;
-            if (count == 3)
-                return nums[i];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > l) {
+                tl = sl;
+                sl = l;
+                l = nums[i];
+            }
+
+            else if (nums[i] > sl && nums[i] < l) {
+                tl = sl;
+                sl = nums[i];
+            }
+
+            else if (nums[i] > tl && nums[i] < sl) {
+                tl = nums[i];
+            }
         }
-        return l;
+        if (tl != Long.MIN_VALUE)
+            return (int) tl;
+        return (int) l;
     }
 }
