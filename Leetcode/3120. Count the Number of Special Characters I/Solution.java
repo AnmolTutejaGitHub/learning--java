fb5eef386ab875511fc5916e6b556b9aa6108eb5
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-05-01 15:42:25 +0530

    leetcode

--- Leetcode/3120. Count the Number of Special Characters I/Solution.java
+++ Leetcode/3120. Count the Number of Special Characters I/Solution.java
@@ -1,16 +1,19 @@
 class Solution {
     public int numberOfSpecialChars(String word) {
         int upper[] = new int[26];
-        int lower[] = new int [26];
+        int lower[] = new int[26];
 
-        for(int i=0;i<word.length();i++){
-            if(word.charAt(i)>='a' && word.charAt(i)<='z') lower[word.charAt(i)-'a']++;
-            else upper[word.charAt(i)-'A']++;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) >= 'a' && word.charAt(i) <= 'z')
+                lower[word.charAt(i) - 'a']++;
+            else
+                upper[word.charAt(i) - 'A']++;
         }
 
         int count = 0;
-        for(int i=0;i<26;i++){
-            if(lower[i]!=0 && upper[i]!=0) count++;
+        for (int i = 0; i < 26; i++) {
+            if (lower[i] != 0 && upper[i] != 0)
+                count++;
         }
         return count;
     }
