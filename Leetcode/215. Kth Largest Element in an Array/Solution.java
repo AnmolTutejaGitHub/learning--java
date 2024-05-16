ece6a3cae9f9658b34042dc6575f4b8cb1eee665
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-05-16 17:09:11 +0530

    leetcode

--- Leetcode/215. Kth Largest Element in an Array/Solution.java
+++ Leetcode/215. Kth Largest Element in an Array/Solution.java
@@ -1,30 +1,43 @@
-//package Leetcode.215. Kth Largest Element in an Array;
+// package Leetcode.215. Kth Largest Element in an Array;
 
-// fucking TIL
+// // fucking TIL
+
+// class Solution {
+// public int findKthLargest(int[] nums, int k) {
+// int ans = -1;
+// int count = 0;
+// while (count < k) {
+// int[] max = maximum(nums);
+// ans = max[0];
+// nums[max[1]] = Integer.MIN_VALUE;
+// count++;
+// }
+// return ans;
+
+// }
+
+// static int[] maximum(int[] arr) {
+// int max = Integer.MIN_VALUE;
+// int maxIndex = -1;
+// for (int i = 0; i < arr.length; i++) {
+// if (arr[i] > max) {
+// max = arr[i];
+// maxIndex = i;
+// }
+// }
+// return new int[] { max, maxIndex };
+// }
+// }
+
+import java.util.PriorityQueue;
 
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        int ans = -1;
-        int count = 0;
-        while (count < k) {
-            int[] max = maximum(nums);
-            ans = max[0];
-            nums[max[1]] = Integer.MIN_VALUE;
-            count++;
-        }
-        return ans;
-
-    }
-
-    static int[] maximum(int[] arr) {
-        int max = Integer.MIN_VALUE;
-        int maxIndex = -1;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-                maxIndex = i;
-            }
-        }
-        return new int[] { max, maxIndex };
+        PriorityQueue<Integer> maxq = new PriorityQueue<>((a, b) -> b - a);
+        for (int i = 0; i < nums.length; i++)
+            maxq.add(nums[i]);
+        for (int i = 1; i < k; i++)
+            maxq.poll();
+        return maxq.peek();
     }
 }
