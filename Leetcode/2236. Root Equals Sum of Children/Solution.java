e799f60069366bed790e857762661ad157d7ff2e
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-05-18 17:16:02 +0530

    leetcode

---
+++ Leetcode/2236. Root Equals Sum of Children/Solution.java
@@ -0,0 +1,24 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public boolean checkTree(TreeNode root) {
+        return root.val == root.left.val + root.right.val;
+    }
+}
