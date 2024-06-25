553d08735f3c201ab3c9d17068a5be92070f8ce3
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-06-25 09:05:10 +0530

    leetcode

---
+++ Leetcode/538. Convert BST to Greater Tree/Solution.java
@@ -0,0 +1,46 @@
+//package Leetcode.538. Convert BST to Greater Tree;
+
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public TreeNode convertBST(TreeNode root) {
+        if (root == null)
+            return null;
+        List<TreeNode> inorder = new ArrayList<>();
+        dfs(root, inorder);
+
+        for (int i = inorder.size() - 2; i >= 0; i--) {
+            inorder.get(i).val = inorder.get(i).val + inorder.get(i + 1).val;
+        }
+
+        return root;
+    }
+
+    public void dfs(TreeNode root, List<TreeNode> inorder) {
+        if (root == null)
+            return;
+
+        dfs(root.left, inorder);
+        inorder.add(root);
+        dfs(root.right, inorder);
+    }
+}
