1d205b9e036627675f34f989474c5b2f1b042757
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-07-05 10:43:41 +0530

    leetcode

---
+++ Leetcode/172. Factorial Trailing Zeroes/Solution.java
@@ -0,0 +1,6 @@
+//package Leetcode.172. Factorial Trailing Zeroes;
+class Solution {
+    public int trailingZeroes(int n) {
+        return n / 5 + n / 25 + (n / 125) + (n / 625) + (n / 3125);
+    }
+}
