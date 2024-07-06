9b42363c04bc4b4ca9231ce03c9aaecb212c6d73
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-07-06 17:04:05 +0530

    leetcode

---
+++ Leetcode/507. Perfect Number/Solution.java
@@ -0,0 +1,12 @@
+//package Leetcode.507. Perfect Number;
+
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int sum = 0;
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0)
+                sum += i;
+        }
+        return sum == num;
+    }
+}
