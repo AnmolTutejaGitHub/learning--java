0e4f3d02cc76da259c627c4377c948e7aa11fdf7
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-07-14 21:30:18 +0530

    leetcode

---
+++ Leetcode/680. Valid Palindrome II/Solution.java
@@ -0,0 +1,27 @@
+//package Leetcode.680. Valid Palindrome II;
+class Solution {
+    public boolean validPalindrome(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+
+        while (l <= r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return isPalindrome(s, l + 1, r) || isPalindrome(s, l, r - 1);
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int l, int r) {
+        while (l <= r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
