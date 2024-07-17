5e0e425e967a3acf0c71a8b2de34f634dbc53990
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-07-17 15:01:42 +0530

    leetcode

---
+++ Leetcode/2974. Minimum Number Game/Solution.java
@@ -0,0 +1,16 @@
+//package Leetcode.2974. Minimum Number Game;
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] numberGame(int[] nums) {
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i += 2) {
+            int temp = nums[i];
+            nums[i] = nums[i + 1];
+            nums[i + 1] = temp;
+        }
+        return nums;
+    }
+}
