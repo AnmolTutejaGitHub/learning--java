44134fbfef9253eb5022b2d706778c5791f76375
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-10-09 07:45:42 +0530

    leetcode

---
+++ Leetcode/921. Minimum Add to Make Parentheses Valid/Solution.java
@@ -0,0 +1,17 @@
+//package Leetcode.921. Minimum Add to Make Parentheses Valid;
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int open = 0;
+        int close = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(')
+                open++;
+            else if (open > 0)
+                open--;
+            else
+                close++;
+        }
+        return open + close;
+    }
+}
