089d8bef7ccc286800a2afb9d3322624f5acb4ff
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2024-10-24 11:17:24 +0530

    leetcode

---
+++ Leetcode/951. Flip Equivalent Binary Trees/Solution.java
@@ -0,0 +1,38 @@
+//package Leetcode.951. Flip Equivalent Binary Trees;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        return dfs(root1, root2);
+    }
+
+    public boolean dfs(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null)
+            return true;
+        if (root1 == null || root2 == null)
+            return false;
+        if (root1.val != root2.val)
+            return false;
+
+        return (dfs(root1.left, root2.left) && dfs(root1.right, root2.right))
+                || (dfs(root1.right, root2.left) && dfs(root1.left, root2.right));
+    }
+}
