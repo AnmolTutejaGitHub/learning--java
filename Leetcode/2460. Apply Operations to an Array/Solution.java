c5f6e89d7381321b6933b64e6c2708c8fe6026fc
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2025-03-01 14:17:22 +0530

    leetcode

--- Leetcode/2460. Apply Operations to an Array/Solution.java
+++ Leetcode/2460. Apply Operations to an Array/Solution.java
@@ -1,25 +1,46 @@
-//package Leetcode.2460. Apply Operations to an Array;
+// package Leetcode.2460. Apply Operations to an Array;
 
+// class Solution {
+// public int[] applyOperations(int[] nums) {
+// for (int i = 0; i < nums.length; i++) {
+// // if(nums[i] == nums[i + 1]){nums[i]*=2;}
+// if (i + 1 < nums.length && nums[i] == nums[i + 1]) {
+// nums[i + 1] = 0;
+// nums[i] *= 2;
+// }
+
+// }
+// for (int i = 0; i < nums.length; i++) {
+
+// for (int j = 0; j < nums.length; j++) {
+// if (nums[j] == 0 && j < nums.length - 1) {
+// int temp = nums[j];
+// nums[j] = nums[j + 1];
+// nums[j + 1] = temp;
+// }
+// }
+// }
+// return nums;
+// }
+// }
+
+// optimised 
 class Solution {
     public int[] applyOperations(int[] nums) {
+        int res[] = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
-            // if(nums[i] == nums[i + 1]){nums[i]*=2;}
             if (i + 1 < nums.length && nums[i] == nums[i + 1]) {
                 nums[i + 1] = 0;
                 nums[i] *= 2;
             }
 
         }
-        for (int i = 0; i < nums.length; i++) {
 
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[j] == 0 && j < nums.length - 1) {
-                    int temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = temp;
-                }
-            }
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0)
+                res[j++] = nums[i];
         }
-        return nums;
+        return res;
     }
 }
