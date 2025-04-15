0f19e34c5ceeb5c14a67623afb373c853a888e7f
AnmolTutejaGitHub <tutejaanmol54@gmail.com>
2025-04-15 21:00:06 +0530

    leetcode

---
+++ Leetcode/240. Search a 2D Matrix II/Solution.java
@@ -0,0 +1,15 @@
+//package Leetcode.240. Search a 2D Matrix II;
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0;
+        int col = matrix[0].length-1;
+        while(row<matrix.length && col>=0){
+            if(matrix[row][col]==target) return true;
+
+            if(matrix[row][col]<target) row++;
+            else col--;
+        }
+        return false;
+    }
+}
